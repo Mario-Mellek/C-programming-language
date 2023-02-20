35a0b0ca076c02297731f2ab60e486f68fd44b3b
Mario-Mellek <mario.samy266@gmail.com>
2023-02-20 02:55:36 +0000

    Added computer selection

--- rock_paper_scissors.c
+++ rock_paper_scissors.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+	#define randnum(min, max) \
+        	((rand() % (int)(((max) + 1) - (min))) + (min))
 
 int main(void)
 {
 	int play()
 	{	
+		int player = 0;
+		int computer = 0;
 		printf("\nRock\tPaper\tScissors");
 		printf("\n1\t2\t3");
 		int input;
@@ -26,7 +33,34 @@ int main(void)
 			default:
 				printf("\nCould detect a selection\n");
 		}
-		printf("-- Computer Selection --");
+		printf("\n\n-- Computer Selection --\n");
+		srand(time(NULL));
+    		switch(randnum(1, 3))
+		{
+			case 1:
+				printf("\nComputer Played Rock\n");
+				input == 3 ? printf("You lost this round") 
+					: input == 2 ? printf("You won this round") 
+					: printf("It's a tie");
+					putchar('\n');
+				break;
+			case 2:
+				printf("\nComputer played Paper\n");
+				input == 1 ? printf("You lost this round") 
+					: input == 3 ? printf("You won this round") 
+					: printf("It's a tie");
+					putchar('\n');
+				break;
+			case 3:
+				printf("\nComputer played Scissors\n");
+				input == 2 ? printf("You lost this round") 
+					: input == 1 ? printf("You won this round") 
+					: printf("It's a tie");
+					putchar('\n');
+				break;
+			default:
+				printf("Computer couldn't play");
+		}
 	}
 	int i;
 	for (i = 1; i <= 5; i++)
