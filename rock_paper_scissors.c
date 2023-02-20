90567b4c9832e97f1ea6f69899603a1c75746a4f
Mario-Mellek <mario.samy266@gmail.com>
2023-02-20 11:50:48 +0000

    Added scores

--- rock_paper_scissors.c
+++ rock_paper_scissors.c
@@ -7,10 +7,12 @@
 
 int main(void)
 {
+
+	int player = 0;
+	int computer = 0;
+
 	int play()
 	{	
-		int player = 0;
-		int computer = 0;
 		printf("\nRock\tPaper\tScissors");
 		printf("\n1\t2\t3");
 		int input;
@@ -39,22 +41,29 @@ int main(void)
 		{
 			case 1:
 				printf("\nComputer Played Rock\n");
-				input == 3 ? printf("You lost this round") 
-					: input == 2 ? printf("You won this round") 
+				input == 3 ? (printf("You lost this round") &&
+						(computer += 1))
+					: input == 2 ? (printf("You won this round") &&
+							(player += 1))
 					: printf("It's a tie");
 					putchar('\n');
 				break;
 			case 2:
 				printf("\nComputer played Paper\n");
-				input == 1 ? printf("You lost this round") 
-					: input == 3 ? printf("You won this round") 
+				input == 1 ? (printf("You lost this round") &&
+						(computer += 1))
+					: input == 3 ? (printf("You won this round") &&
+							(player += 1))
+
 					: printf("It's a tie");
 					putchar('\n');
 				break;
 			case 3:
 				printf("\nComputer played Scissors\n");
-				input == 2 ? printf("You lost this round") 
-					: input == 1 ? printf("You won this round") 
+				input == 2 ? (printf("You lost this round") && 
+						(computer +=1))
+					: input == 1 ? (printf("You won this round") &&
+							(player +=1))
 					: printf("It's a tie");
 					putchar('\n');
 				break;
@@ -62,11 +71,15 @@ int main(void)
 				printf("Computer couldn't play");
 		}
 	}
+
 	int i;
+
 	for (i = 1; i <= 5; i++)
 	{
 		printf("\n\n------------Round number %d------------", i);
 		play();
+		printf("Player: %d\nComputer: %d", player, computer);
+		putchar('\n');
 	}
 	return (0);
 }
